9577cf68fb779f7417ccca1965c5064392c5619d
Jose Paitamala <jose.paitamala@borngroup.com>
2023-01-21 17:35:53 +0000

    Add heroku port settings

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ searchForm.addEventListener('submit', (e) => {
 
     messageOne.innerHTML = 'Loading...';
 
-    fetch('http://localhost:3000/weather?address='+location).then( (response) => {
+    fetch('/weather?address='+location).then( (response) => {
         response.json().then( (data) => {
             if (data.error) {
                 messageOne.innerHTML = '';
